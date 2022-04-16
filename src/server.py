a107bd171abefda07d0d5c47f8f14bbe36234f0e
alvaro sanchez <asanchez@mrmilu.com>
2022-04-16 11:34:20 +0200

    prime-request-context middleware added

--- src/server.py
+++ src/server.py
@@ -1,9 +1,16 @@
+from uuid import uuid4
+
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import HTTPResponse, text
 
 
 app = Sanic("MyApp")
+
+
+@app.middleware("request")
+async def prime_request_context(request: Request):
+    request.ctx.traceId = uuid4()
 
 
 @app.get("/")
